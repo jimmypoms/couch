8ed191c78bc73c76990de2fbba54ca9cf1a23070
Dominik Spengler <jimmypoms@gmail.com>
2017-02-01 14:43:46 +0100

    print exception on segfault

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -7,16 +7,83 @@
 #include "couch/playbackhandler.h"
 #include "couch/provider.h"
 
-#include <qglobal.h>
 #include <qguiapplication.h>
 #include <qobject.h>
 #include <qqmlapplicationengine.h>
 #include <qqmlcontext.h>
-#include <qscreen.h>
+#include <qstring.h>
 #include <qurl.h>
+
+#include <execinfo.h>
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ucontext.h>
+#include <unistd.h>
+
+/* This structure mirrors the one found in /usr/include/asm/ucontext.h */
+typedef struct _sig_ucontext {
+    unsigned long     uc_flags;
+    struct ucontext   *uc_link;
+    stack_t           uc_stack;
+    struct sigcontext uc_mcontext;
+    sigset_t          uc_sigmask;
+} sig_ucontext_t;
+
+void crit_err_hdlr(int sig_num, siginfo_t * info, void * ucontext) {
+    void *             array[50];
+    void *             caller_address;
+    char **            messages;
+    int                size, i;
+    sig_ucontext_t *   uc;
+
+    uc = (sig_ucontext_t *)ucontext;
+
+    /* Get the address at the time the signal was raised */
+    #if defined(__i386__) // gcc specific
+    caller_address = (void *) uc->uc_mcontext.eip; // EIP: x86 specific
+    #elif defined(__x86_64__) // gcc specific
+    caller_address = (void *) uc->uc_mcontext.rip; // RIP: x86_64 specific
+    #else
+    #error Unsupported architecture. // TODO: Add support for other arch.
+    #endif
+
+    fprintf(stderr, "\n");
+
+    if (sig_num == SIGSEGV)
+        fprintf(stderr, "signal %d (%s), address is %p from %p\n",sig_num, strsignal(sig_num), info->si_addr,(void *)caller_address);
+    else
+        fprintf(stderr, "signal %d (%s)\n",sig_num, strsignal(sig_num));
+
+    size = backtrace(array, 50);
+    /* overwrite sigaction with caller's address */
+    array[1] = caller_address;
+    messages = backtrace_symbols(array, size);
+    /* skip first stack frame (points here) */
+    for (i = 1; i < size && messages != NULL; ++i) {
+        fprintf(stderr, "[bt]: (%d) %s\n", i, messages[i]);
+    }
+
+    free(messages);
+
+    exit(EXIT_FAILURE);
+}
+
+void installSignal(int __sig) {
+    struct sigaction sigact;
+    sigact.sa_sigaction = crit_err_hdlr;
+    sigact.sa_flags = SA_RESTART | SA_SIGINFO;
+    if (sigaction(__sig, &sigact, (struct sigaction *)NULL) != 0) {
+        fprintf(stderr, "error setting signal handler for %d (%s)\n",__sig, strsignal(__sig));
+        exit(EXIT_FAILURE);
+    }
+}
 
 int main(int argc, char *argv[])
 {
+    installSignal(SIGSEGV);
+
     QGuiApplication app(argc, argv);
     Couch couch;
     CouchPlayer player;
